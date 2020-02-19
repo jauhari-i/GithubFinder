a92011734bfce5e869edc709d1e94c32c60771a7
jauhari-i <muhammad_irfan_27rpl@student.smktelkom-mlg.sch.id>
2020-02-19 14:54:54 +0700

    Updates

--- precache-manifest.81d60e2a9147dfcdd6ebd369a3e223df.js
+++ precache-manifest.81d60e2a9147dfcdd6ebd369a3e223df.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c680db0e78bc3170c9490d556abd697a",
+    "revision": "e76dadb322a0f7b5387e0867f131ce37",
     "url": "/GithubFinder/index.html"
   },
   {
-    "revision": "48a8feb7776876c02bb6",
+    "revision": "783d6c1fd0ba9d90788f",
     "url": "/GithubFinder/static/css/main.b93e5a4d.chunk.css"
   },
   {
-    "revision": "2900a66f9a56833e69a4",
-    "url": "/GithubFinder/static/js/2.6308bc22.chunk.js"
+    "revision": "0f64492d3f6d6ac54e46",
+    "url": "/GithubFinder/static/js/2.1a7150bd.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/GithubFinder/static/js/2.6308bc22.chunk.js.LICENSE.txt"
+    "url": "/GithubFinder/static/js/2.1a7150bd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "48a8feb7776876c02bb6",
-    "url": "/GithubFinder/static/js/main.3715804a.chunk.js"
+    "revision": "783d6c1fd0ba9d90788f",
+    "url": "/GithubFinder/static/js/main.d466db09.chunk.js"
   },
   {
     "revision": "ace50c554c8bbdc484df",
